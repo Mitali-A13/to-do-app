6aac7f616e8a835a506de63df6799eb39e633c58
Mitali Awasthi <mitaliawasthi@MacBook-Air-9.local>
2025-11-09 00:57:45 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// Select DOM elements
+const input = document.getElementById('todo-input');
+const addBtn = document.getElementById('add-btn');
+const todoList = document.getElementById('todo-list');
+
+// Try to load saved todos from localStorage (if any)
